54a9ed652ea1e8cdace968885cb17e17fa539afb
Christopher Serr <christopher.serr@gmail.com>
2017-08-13 16:26:12 +0200

    Use Webpack Dev Server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,23 @@
+const path = require('path');
+
+const basePath = __dirname;
+const distPath = path.join(basePath, "dist");
+
 module.exports = {
     entry: "./src/index.tsx",
     output: {
         filename: "bundle.js",
-        path: __dirname + "/dist"
+        path: distPath,
     },
 
     // Enable sourcemaps for debugging webpack's output.
     // devtool: "source-map",
+
+    devServer: {
+        contentBase: basePath,
+        compress: true,
+        port: 8080,
+    },
 
     resolve: {
         // Add '.ts' and '.tsx' as resolvable extensions.
@@ -40,7 +51,7 @@ module.exports = {
 
         // preLoaders: [
         //     // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        //     // { test: /\.js$/, loader: "source-map-loader" }
+        //     { test: /\.js$/, loader: "source-map-loader" }
         // ]
     },
 
