32f7428769a77ad0bc552dde57d1520a436b8048
Christopher Serr <christopher.serr@gmail.com>
2018-02-02 02:36:13 +0100

    Begin working on using WebAssembly

--- Makefile
+++ Makefile
@@ -6,6 +6,12 @@ wasm: bindings
 	@make wasm -C livesplit-core/js
 	@cp livesplit-core/js/livesplit.js src/livesplit_core.js
 
+wasm-unknown:
+	@make bindings -C livesplit-core/capi/js
+	@cp livesplit-core/capi/bindings/wasm/livesplit_core.ts src/livesplit.ts
+	@make wasm-unknown -C livesplit-core/capi/js
+	@cp livesplit-core/capi/js/livesplit_core.wasm src/livesplit_core.wasm
+
 debug: bindings
 	@make debug -C livesplit-core/js
 	@cp livesplit-core/js/livesplit.js src/livesplit_core.js
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import { load } from "./livesplit";
 import { LiveSplit } from "./ui/LiveSplit";
 
 import "./css/font-awesome.css";
 import "./css/livesplit.css";
 
-ReactDOM.render(
-    <div>
-        <LiveSplit />
-    </div>,
-    document.getElementById("base"),
-);
+async function run() {
+    await load("src/livesplit_core.wasm");
+
+    ReactDOM.render(
+        <div>
+            <LiveSplit />
+        </div>,
+        document.getElementById("base"),
+    );
+}
+
+run();
