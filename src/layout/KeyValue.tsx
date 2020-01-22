2073800c0d8b6c82e417c0b11ef73dae713bb9e0
wooferzfg <spapushin@gmail.com>
2020-01-21 23:30:25 -0600

    Allow KeyValue components to show two rows

--- src/layout/KeyValue.tsx
+++ src/layout/KeyValue.tsx
@@ -9,33 +9,53 @@ export interface Props { state: LiveSplit.KeyValueComponentStateJson }
 
 export default class KeyValue extends React.Component<Props> {
     public render() {
+        const keyCell = <td
+            className="key-value-key"
+            style={{
+                color: map(this.props.state.key_color, colorToCss),
+            }}
+        >
+            {this.props.state.key}
+        </td>;
+
+        const valueCell = <td
+            className="key-value-value time"
+            style={{
+                color: map(this.props.state.value_color, colorToCss),
+            }}
+        >
+            {this.props.state.value}
+        </td>;
+
+        let keyValueRows;
+        if (this.props.state.display_two_rows) {
+            keyValueRows = <>
+                <tr>
+                    {keyCell}
+                </tr>
+                <tr>
+                    {valueCell}
+                </tr>
+            </>;
+        } else {
+            keyValueRows = <>
+                <tr>
+                    {keyCell}
+                    {valueCell}
+                </tr>
+            </>;
+        }
+
         return (
             <div
-                className="key-value"
+                className={`key-value ${this.props.state.display_two_rows ? " key-value-two-rows" : ""}`}
                 style={{
                     background: gradientToCss(this.props.state.background),
                 }}
             >
                 <table>
                     <tbody>
-                        <tr>
-                            <td
-                                className="key-value-key"
-                                style={{
-                                    color: map(this.props.state.key_color, colorToCss),
-                                }}
-                            >
-                                {this.props.state.key}
-                            </td>
-                            <td
-                                className="key-value-value time"
-                                style={{
-                                    color: map(this.props.state.value_color, colorToCss),
-                                }}
-                            >
-                                {this.props.state.value}
-                            </td>
-                        </tr>
+                        {keyValueRows}
                     </tbody>
                 </table>
             </div>
