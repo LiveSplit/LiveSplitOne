ebcac464b44300a78868342075ae1e4f8a02a62b
wooferzfg <spapushin@gmail.com>
2020-02-21 07:53:17 -0600

    Remove commented out lines from webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -97,14 +97,5 @@ module.exports = (env, argv) => {
         },
 
         mode: isProduction ? "production" : "development",
-
-        // When importing a module whose path matches one of the following, just
-        // assume a corresponding global variable exists and use that instead.
-        // This is important because it allows us to avoid bundling all of our
-        // dependencies, which allows browsers to cache those libraries between builds.
-        // externals: {
-        //     "react": "React",
-        //     "react-dom": "ReactDOM",
-        // },
     };
 };
