eb5dfd71da56852c9c0b1ab63bc1156428e2a4b2
wooferzfg <spapushin@gmail.com>
2020-02-23 14:08:03 -0600

    Fix DragAutoRefreshLayout class name

--- src/layout/DragAutoRefreshLayout.tsx
+++ src/layout/DragAutoRefreshLayout.tsx
@@ -21,7 +21,7 @@ export interface State {
     hoverIndex: Option<number>,
 }
 
-export default class AutoRefreshLayout extends React.Component<Props, State> {
+export default class DragAutoRefreshLayout extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
 
