2063a99b09908732230ae4baf3900425f1075b9f
Christopher Serr <christopher.serr@gmail.com>
2024-10-22 20:19:34 +0200

    Enable Multivalue again
    With LLVM 19 they explicitly require using the `experimental-mv` ABI in
    addition to the `multivalue` LLVM feature (the latter is enabled by
    default now). Enabling the ABI is non-trivial and requires a full on
    custom Rust target. This adds `wasm32-multivalue.json` which is
    identical to `wasm32-unknown-unknown` except it sets the
    `experimental-mv` ABI. The target JSON format is very experimental by
    itself, so we may need to keep this up to date from time to time.

--- buildCore.js
+++ buildCore.js
@@ -2,18 +2,21 @@ import { execSync } from "child_process";
 import fs from "fs";
 
 let toolchain = "";
-let targetFolder = "debug";
+let profile = "debug";
 let cargoFlags = "";
-let rustFlags = "-C target-feature=+bulk-memory,+mutable-globals,+nontrapping-fptoint,+sign-ext,+simd128,+extended-const";
+let rustFlags =
+    "-C target-feature=+bulk-memory,+mutable-globals,+nontrapping-fptoint,+sign-ext,+simd128,+extended-const,+multivalue";
 let wasmBindgenFlags = "";
+let target = "wasm32-unknown-unknown";
+let targetFolder = target;
 
 if (process.argv.some((v) => v === "--max-opt")) {
     // Do a fully optimized build ready for deployment.
-    targetFolder = "max-opt";
+    profile = "max-opt";
     cargoFlags = "--profile max-opt";
 } else if (process.argv.some((v) => v === "--release")) {
     // Do an optimized build.
-    targetFolder = "release";
+    profile = "release";
     cargoFlags = "--release";
 } else {
     // Do a debug build.
@@ -23,19 +26,28 @@ if (process.argv.some((v) => v === "--max-opt")) {
 // Use WASM features that may not be supported by all the browsers.
 if (process.argv.some((v) => v === "--unstable")) {
     // Relaxed SIMD is not supported by Firefox and Safari yet.
-    // Tail calls are not supported by Safari and wasm-bindgen yet.
-    rustFlags += ",+relaxed-simd"; //,+tail-call";
+    rustFlags += ",+relaxed-simd";
+
+    // Tail calls are not supported by Safari yet.
+    rustFlags += ",+tail-call";
+
+    // Reference types are broken in webpack (or rather its underlying webassemblyjs):
+    // https://github.com/LiveSplit/LiveSplitOne/issues/630
+    // wasmBindgenFlags += " --reference-types";
 }
 
 // Use the nightly toolchain, which enables some more optimizations.
 if (process.argv.some((v) => v === "--nightly")) {
     toolchain = "+nightly";
-    cargoFlags += " -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort";
-    rustFlags += ",+multivalue -Z wasm-c-abi=spec";
+    cargoFlags +=
+        " -Z build-std=std,panic_abort -Z build-std-features=panic_immediate_abort";
+    rustFlags += " -Z wasm-c-abi=spec";
+    target = "../wasm32-multivalue.json";
+    targetFolder = "wasm32-multivalue";
 
     // Virtual function elimination requires LTO, so we can only do it for
     // max-opt builds.
-    if (targetFolder == "max-opt") {
+    if (profile == "max-opt") {
         // Seems like cargo itself calls rustc to check for file name patterns,
         // but it forgets to pass the LTO flag that we specified in the
         // Cargo.toml, so the virtual-function-elimination complains that it's
@@ -45,33 +57,30 @@ if (process.argv.some((v) => v === "--nightly")) {
     }
 }
 
-execSync(
-    `cargo ${toolchain} run`,
-    {
-        cwd: "livesplit-core/capi/bind_gen",
-        stdio: "inherit",
-    },
-);
+execSync(`cargo ${toolchain} run`, {
+    cwd: "livesplit-core/capi/bind_gen",
+    stdio: "inherit",
+});
 
 execSync(
-    `cargo ${toolchain} rustc -p livesplit-core-capi --crate-type cdylib --features wasm-web,web-rendering --target wasm32-unknown-unknown ${cargoFlags}`,
+    `cargo ${toolchain} rustc -p livesplit-core-capi --crate-type cdylib --features wasm-web,web-rendering --target ${target} ${cargoFlags}`,
     {
         cwd: "livesplit-core",
         stdio: "inherit",
         env: {
             ...process.env,
-            'RUSTFLAGS': rustFlags,
+            RUSTFLAGS: rustFlags,
         },
-    },
+    }
 );
 
 execSync(
-    `wasm-bindgen ${wasmBindgenFlags} livesplit-core/target/wasm32-unknown-unknown/${targetFolder}/livesplit_core.wasm --out-dir src/livesplit-core`,
+    `wasm-bindgen ${wasmBindgenFlags} livesplit-core/target/${targetFolder}/${profile}/livesplit_core.wasm --out-dir src/livesplit-core`,
     {
         stdio: "inherit",
-    },
+    }
 );
 
-fs
-    .createReadStream("livesplit-core/capi/bindings/wasm_bindgen/index.ts")
-    .pipe(fs.createWriteStream("src/livesplit-core/index.ts"));
+fs.createReadStream("livesplit-core/capi/bindings/wasm_bindgen/index.ts").pipe(
+    fs.createWriteStream("src/livesplit-core/index.ts")
+);
