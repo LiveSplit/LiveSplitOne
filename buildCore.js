663f60b7baf24b57f00934f2984c05f327a7e9bc
Christopher Serr <christopher.serr@gmail.com>
2024-11-06 00:55:29 +0100

    Switch to the `web` Target of `wasm-bindgen` (#989)
    This allows us to skip letting `webpack` parse the wasm files, which
    means we are not blocked by its limitations anymore. This allows us to
    finally start using reference types.

--- buildCore.js
+++ buildCore.js
@@ -6,7 +6,7 @@ let profile = "debug";
 let cargoFlags = "";
 let rustFlags =
     "-C target-feature=+bulk-memory,+mutable-globals,+nontrapping-fptoint,+sign-ext,+simd128,+extended-const,+multivalue";
-let wasmBindgenFlags = "";
+let wasmBindgenFlags = "--encode-into always --target web --reference-types";
 let target = "wasm32-unknown-unknown";
 let targetFolder = target;
 
@@ -28,12 +28,8 @@ if (process.argv.some((v) => v === "--unstable")) {
     // Relaxed SIMD is not supported by Firefox and Safari yet.
     rustFlags += ",+relaxed-simd";
 
-    // Tail calls are not supported by Safari yet.
+    // Tail calls are not supported by Safari yet until 18.2 (early December).
     rustFlags += ",+tail-call";
-
-    // Reference types are broken in webpack (or rather its underlying webassemblyjs):
-    // https://github.com/LiveSplit/LiveSplitOne/issues/630
-    // wasmBindgenFlags += " --reference-types";
 }
 
 // Use the nightly toolchain, which enables some more optimizations.
@@ -81,6 +77,10 @@ execSync(
     }
 );
 
-fs.createReadStream("livesplit-core/capi/bindings/wasm_bindgen/index.ts").pipe(
-    fs.createWriteStream("src/livesplit-core/index.ts")
-);
+fs.createReadStream(
+    "livesplit-core/capi/bindings/wasm_bindgen/web/index.ts"
+).pipe(fs.createWriteStream("src/livesplit-core/index.ts"));
+
+fs.createReadStream(
+    "livesplit-core/capi/bindings/wasm_bindgen/web/preload.ts"
+).pipe(fs.createWriteStream("src/livesplit-core/preload.ts"));
